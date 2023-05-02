8abf91ad91661221cc36b9a63b0c8b26a8b51d85
KipEric <kipkoecheric97@gmail.com>
2023-05-02 16:37:15 +0300

    correction3

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,27 +2,35 @@
 """Flask script"""
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response, render_template, url_for
 from models import storage
 from os import getenv
 from api.v1.views import app_views
+from flask_cors import CORS, cross_origin
+from flasgger import Swagger
+from werkzeug.exceptions import HTTPException
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+swagger = Swagger(app)
+app.register_blueprint(app_views)
 app.url_mao.strict_slashes=False
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
-def tear_down(self):
+def tear_down(exception):
     """API status"""
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
+def page_not_found(exception):
     """Function to handle error code 404"""
-    return jsonify({'error': 'Not found'}), 404
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
 
 
 if __name__ == "__main__":
